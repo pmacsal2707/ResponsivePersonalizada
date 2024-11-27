8ee26cf99982d2dc2de61efc7c5014cd48a45577
Pablo <pmacsal2707@g.educaand.es>
2024-11-27 14:53:33 +0100

    cambios

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -6,12 +6,14 @@ import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
 
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    HttpClientModule,
     HomePageRoutingModule
   ],
   declarations: [HomePage]
